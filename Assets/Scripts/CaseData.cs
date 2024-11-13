c1d864b344b6ce93b7db3454d4ff6b354640cd3f
wilzzuu <elias.w.terho@gmail.com>
2024-11-13 23:44:56 +0200

    Cleanup

--- Assets/Scripts/CaseData.cs
+++ Assets/Scripts/CaseData.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Serialization;
 
-[CreateAssetMenu(fileName = "New CrateData", menuName = "Case Data", order = 51)]
+[CreateAssetMenu(fileName = "New CaseData", menuName = "Case Data", order = 51)]
 public class CaseData : ScriptableObject
 {
-    public string ID;
-    public string Name;
-    public float Price;
-    public List<ItemData> Items;
+    public string id;
+    public new string name;
+    public float price;
+    public List<ItemData> items;
 }
