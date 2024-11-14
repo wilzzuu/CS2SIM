b8ecfc405747eec54a90e9586ed847ebef8a97c3
wilzzuu <elias.w.terho@gmail.com>
2024-11-14 09:53:29 +0200

    Update SceneSwitch.cs

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -18,64 +18,89 @@ public class SceneSwitch : MonoBehaviour
         SceneManager.LoadScene(3);
     }
 
-    public void LoadTradingScene()
+    public void LoadGameSelector()
     {
         SceneManager.LoadScene(4);
     }
 
-    public void LoadCaseOpeningScene()
+    public void LoadTradingScene()
     {
         SceneManager.LoadScene(5);
     }
 
-    public void LoadContractsScene()
+    public void LoadCaseOpeningScene()
     {
         SceneManager.LoadScene(6);
     }
-
-    public void LoadCoinFlipScene()
+    
+    public void LoadCaseBattleScene()
     {
         SceneManager.LoadScene(7);
     }
 
-    public void LoadRouletteScene()
+    public void LoadContractsScene()
     {
         SceneManager.LoadScene(8);
     }
 
-    public void LoadUpgraderScene()
+    public void LoadCoinFlipScene()
     {
         SceneManager.LoadScene(9);
     }
 
-    public void LoadCrashScene()
+    public void LoadRouletteScene()
     {
         SceneManager.LoadScene(10);
     }
 
-    public void LoadPlinkoScene()
+    public void LoadUpgraderScene()
     {
         SceneManager.LoadScene(11);
     }
 
-    public void LoadClickerScene()
+    public void LoadCrashScene()
     {
         SceneManager.LoadScene(12);
     }
 
-    public void LoadHighLowScene()
+    public void LoadPlinkoScene()
     {
         SceneManager.LoadScene(13);
     }
 
-    public void LoadTowerScene()
+    public void LoadClickerScene()
     {
         SceneManager.LoadScene(14);
     }
 
-    public void LoadSettingsScene()
+    public void LoadHighLowScene()
     {
         SceneManager.LoadScene(15);
+    }
+
+    public void LoadTowerScene()
+    {
+        SceneManager.LoadScene(16);
+    }
+    
+    public void LoadSettingsScene()
+    {
+        SceneManager.LoadScene(17);
+    }
+
+    public void LoadAchievementsScene()
+    {
+        SceneManager.LoadScene(18);
+    }
+
+    public void LoadRewardsScene()
+    {
+        SceneManager.LoadScene(19);
+    }
+
+    public void LoadNotificationsScene()
+    {
+        SceneManager.LoadScene(20);
     }
 
     public void ExitApplication()
