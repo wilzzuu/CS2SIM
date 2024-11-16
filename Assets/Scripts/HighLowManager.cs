9ee883f7935a23225fa674ced9ae5ef79fa35888
wilzzuu <elias.w.terho@gmail.com>
2024-11-16 17:43:13 +0200

    Created HighLow manager

---
+++ Assets/Scripts/HighLowManager.cs
@@ -0,0 +1,215 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighLowManager : MonoBehaviour
+{
+    public Transform previousItemContainer;
+    public Transform nextItemContainer;
+    public GameObject highLowItemPrefab;
+    public TMP_InputField betAmountInput;
+    public Button guessHighButton;
+    public Button guessLowButton;
+    public Button startRoundButton;
+    public Button cashOutButton;
+    public TextMeshProUGUI resultText;
+
+    private float _previousItemPrice;
+    private float _nextItemPrice;
+    private float _betAmount;
+    private int _roundNumber;
+    
+    private List<ItemData> _allItems = new List<ItemData>();
+
+    void Start()
+    {
+        guessHighButton.onClick.AddListener(GuessHigh);
+        guessLowButton.onClick.AddListener(GuessLow);
+        cashOutButton.onClick.AddListener(CashOut);
+        startRoundButton.onClick.AddListener(StartRound);
+        guessHighButton.interactable = false;
+        guessLowButton.interactable = false;
+        cashOutButton.interactable = false;
+        startRoundButton.interactable = true;
+        betAmountInput.interactable = true;
+        
+        _allItems = Resources.LoadAll<ItemData>("ItemAssets").ToList();
+    }
+
+    private void SetupFirstItem()
+    {
+        resultText.text = "";
+        _previousItemPrice = 0f;
+        _nextItemPrice = 0f;
+        var randomItem = GetRandomItem();
+        PopulatePreviousItem(randomItem);
+        PopulateNextHiddenItem();
+    }
+    
+    private void PopulateNextHiddenItem()
+    {
+        foreach (Transform child in nextItemContainer)
+        {
+            Destroy(child.gameObject);
+        }
+        
+        GameObject nextItem = Instantiate(highLowItemPrefab, nextItemContainer);
+        Image itemImage = nextItem.transform.Find("ItemImage").GetComponent<Image>();
+        Image rarityImage = nextItem.transform.Find("RarityImage").GetComponent<Image>();
+        TextMeshProUGUI gunText = nextItem.transform.Find("GunText").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI nameText = nextItem.transform.Find("NameText").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI priceText = nextItem.transform.Find("PriceText").GetComponent<TextMeshProUGUI>();
+
+        itemImage.sprite = Resources.Load<Sprite>($"HiddenItem");
+        rarityImage.gameObject.SetActive(false);
+        gunText.gameObject.SetActive(false);
+        nameText.gameObject.SetActive(false);
+        priceText.gameObject.SetActive(false);
+    }
+
+    private void PopulateNextRevealedItem(ItemData item)
+    {
+        foreach (Transform child in nextItemContainer)
+        {
+            Destroy(child.gameObject);
+        }
+        
+        GameObject revealItem = Instantiate(highLowItemPrefab, nextItemContainer);
+        Image itemImage = revealItem.transform.Find("ItemImage").GetComponent<Image>();
+        Image rarityImage = revealItem.transform.Find("RarityImage").GetComponent<Image>();
+        TextMeshProUGUI gunText = revealItem.transform.Find("GunText").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI nameText = revealItem.transform.Find("NameText").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI priceText = revealItem.transform.Find("PriceText").GetComponent<TextMeshProUGUI>();
+    
+        rarityImage.gameObject.SetActive(true);
+        gunText.gameObject.SetActive(true);
+        nameText.gameObject.SetActive(true);
+        priceText.gameObject.SetActive(true);
+        
+        itemImage.sprite = Resources.Load<Sprite>($"ItemImages/{item.id}");
+        rarityImage.sprite = Resources.Load<Sprite>($"RarityImages/{item.rarity}");
+        gunText.text = item.gun;
+        nameText.text = item.name;
+        priceText.text = $"{item.price:F2}€";
+        
+        _nextItemPrice = item.price;
+    }
+
+    private void PopulatePreviousItem(ItemData item)
+    {
+        foreach (Transform child in previousItemContainer)
+        {
+            Destroy(child.gameObject);
+        }
+        
+        GameObject prevItem = Instantiate(highLowItemPrefab, previousItemContainer);
+        Image itemImage = prevItem.transform.Find("ItemImage").GetComponent<Image>();
+        Image rarityImage = prevItem.transform.Find("RarityImage").GetComponent<Image>();
+        TextMeshProUGUI gunText = prevItem.transform.Find("GunText").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI nameText = prevItem.transform.Find("NameText").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI priceText = prevItem.transform.Find("PriceText").GetComponent<TextMeshProUGUI>();
+
+        itemImage.sprite = Resources.Load<Sprite>($"ItemImages/{item.id}");
+        rarityImage.sprite = Resources.Load<Sprite>($"RarityImages/{item.rarity}");
+        gunText.text = item.gun;
+        nameText.text = item.name;
+        priceText.text = $"{item.price:F2}€";
+        
+        _previousItemPrice = item.price;
+    }
+    
+    private ItemData GetRandomItem()
+    {
+        int randomIndex = Random.Range(0, _allItems.Count);
+        return _allItems[randomIndex];
+    }
+
+    private void StartRound()
+    {
+        foreach (Transform child in previousItemContainer)
+        {
+            Destroy(child.gameObject);
+        }
+        
+        foreach (Transform child in nextItemContainer)
+        {
+            Destroy(child.gameObject);
+        }
+        
+        if (float.TryParse(betAmountInput.text, out _betAmount) && _betAmount > 0 &&
+            _betAmount <= PlayerManager.Instance.GetPlayerBalance())
+        {
+            PlayerManager.Instance.DeductCurrency(_betAmount);
+            guessHighButton.interactable = true;
+            guessLowButton.interactable = true;
+            startRoundButton.interactable = false;
+            betAmountInput.interactable = false;
+            
+            SetupFirstItem();
+        }
+        else
+        {
+            Debug.LogWarning("Invalid bet amount or not enough balance.");
+        }
+    }
+
+    private void GuessHigh()
+    {
+        var randomItem = GetRandomItem();
+        PopulateNextRevealedItem(randomItem);
+        
+        if (_nextItemPrice > _previousItemPrice)
+        {
+            if (!cashOutButton.interactable) cashOutButton.interactable = true;
+            _roundNumber++;
+            resultText.text = $"You guessed right, round {_roundNumber}!";
+            PopulateNextHiddenItem();
+            PopulatePreviousItem(randomItem);
+        }
+        else
+        {
+            resultText.text = "You guessed wrong and lost.";
+            GameOver();
+        }
+    }
+
+    private void GuessLow()
+    {
+        var randomItem = GetRandomItem();
+        PopulateNextRevealedItem(randomItem);
+        
+        if (_nextItemPrice < _previousItemPrice)
+        {
+            if (!cashOutButton.interactable) cashOutButton.interactable = true;
+            _roundNumber++;
+            resultText.text = $"You guessed right on round {_roundNumber}!";
+            PopulateNextHiddenItem();
+            PopulatePreviousItem(randomItem);
+        }
+        else
+        {
+            resultText.text = "You guessed wrong and lost.";
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        _roundNumber = 0;
+        guessHighButton.interactable = false;
+        guessLowButton.interactable = false;
+        cashOutButton.interactable = false;
+        startRoundButton.interactable = true;
+        betAmountInput.interactable = true;
+    }
+    
+    private void CashOut()
+    {
+        float winnings = _betAmount * _roundNumber;
+        PlayerManager.Instance.AddCurrency(winnings);
+        resultText.text = $"You cashed out {winnings:F2}€";
+        GameOver();
+    }
+}
