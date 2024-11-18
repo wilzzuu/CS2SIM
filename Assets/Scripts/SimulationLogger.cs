4a4dca7352922f41437e053bb9493391155bfa99
wilzzuu <elias.w.terho@gmail.com>
2024-11-19 01:57:31 +0200

    Added simulation duration logging

--- Assets/Scripts/SimulationLogger.cs
+++ Assets/Scripts/SimulationLogger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using UnityEngine;
 
 public class SimulationLogger : MonoBehaviour
@@ -21,6 +22,8 @@ public class SimulationLogger : MonoBehaviour
     [ContextMenu("Run Case Simulation")]
     private void RunSimulation()
     {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        
         Dictionary<ItemData, int> itemCounts = new Dictionary<ItemData, int>();
         Dictionary<string, int> rarityCounts = new Dictionary<string, int>();
 
@@ -44,9 +47,13 @@ public class SimulationLogger : MonoBehaviour
                 }
             }
         }
+        
+        stopwatch.Stop();
 
         // Log the results to the StringBuilder instead of Debug.Log
         _logBuilder.AppendLine($"Simulation of {numberOfSimulations} case openings completed.\n");
+        _logBuilder.AppendLine($"Simulation Duration: {stopwatch.ElapsedMilliseconds} ms\n");
+
         itemCounts = itemCounts.OrderBy(item =>
                 RarityOrder.RarityOrderList.ContainsKey(item.Key.rarity) ? RarityOrder.RarityOrderList[item.Key.rarity] : int.MaxValue)
             .ToDictionary(data => data.Key, data => data.Value);
@@ -72,7 +79,7 @@ public class SimulationLogger : MonoBehaviour
                 averagePercentageDifList.Add(percentageDif);
                 
                 _logBuilder.AppendLine($"Condition: {entry.Key}"); 
-                _logBuilder.AppendLine($"Appeared: {entry.Value} times ({percentage:F5}%) with a difference of {percentageDif:F3}%\n");
+                _logBuilder.AppendLine($"Appeared: {entry.Value} times ({percentage:F5}%) with a difference of {percentageDif:F5}%\n");
             }
             else
             {
@@ -96,6 +103,6 @@ public class SimulationLogger : MonoBehaviour
         string filePath = Path.Combine(simulationPath, fileName);
         
         File.WriteAllText(filePath, _logBuilder.ToString());
-        Debug.Log($"Simulation results saved to: {filePath}");
+        UnityEngine.Debug.Log($"Simulation results saved to: {filePath}");
     }
 }
